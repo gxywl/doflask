5ee71ea42997b9647a5318fd201e190cd38ececa
gxywl <gxywl@163.com>
2018-02-27 00:59:12 +0800

    03 使用bootstrap前端框架 加入错误页

--- doflask.py
+++ doflask.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
@@ -12,8 +14,15 @@ def index():
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html',name=name)
+    return render_template('user.html', name=name)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'),500
 
 if __name__ == '__main__':
     manager.run()
