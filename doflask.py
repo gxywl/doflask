3a4577bdf59534baa25025978c4ab7fb8ae75aac
gxywl <gxywl@163.com>
2018-02-27 19:13:12 +0800

    06 使用WTF表单

--- doflask.py
+++ doflask.py
@@ -1,35 +1,53 @@
 #!/usr/bin/python
-#_*_ coding:utf8 _*_
+# _*_ coding:utf8 _*_
 
 from datetime import datetime
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'hard go guess string'
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
-moment=Moment(app)
+moment = Moment(app)
 
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField("What is your name?", validators=[DataRequired()])
+    submit = SubmitField('submit')
+
+
+@app.route('/', methods=['Get', 'Post'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
 
+
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'),404
+    return render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'),500
+    return render_template('500.html'), 500
+
 
 if __name__ == '__main__':
     manager.run()
