e7895893ab8f106454598ea0cfe75df2f2d79538
gxywl <gxywl@163.com>
2018-02-28 00:47:20 +0800

    10 用shell发邮件成功 smtp.qq.com 587

--- doflask.py
+++ doflask.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 import os
 from flask import Flask, render_template, session, redirect, url_for, flash
+from flask_mail import Mail
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager, Shell
 from flask_bootstrap import Bootstrap
@@ -24,11 +25,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'da
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
+app.config['MAIL_SERVER'] = 'smtp.qq.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
-migrate=Migrate(app,db)
+migrate = Migrate(app, db)
+mail = Mail(app)
 
 
 class Role(db.Model):
@@ -58,7 +66,8 @@ def make_shell_context():
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
 
-manager.add_command('db',MigrateCommand)
+manager.add_command('db', MigrateCommand)
+
 
 class NameForm(FlaskForm):
     name = StringField("What is your name?", validators=[DataRequired()])
