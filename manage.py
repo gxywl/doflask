548ae373190824f43a5d6295abdd8ace454ec81a
gxywl <gxywl@163.com>
2018-03-01 01:52:40 +0800

    14 可运行出现主页,但测试还出错,不能通过

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import unittest
 
 from flask_script import Manager
 
@@ -8,6 +9,12 @@ from app import create_app
 app=create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 manager = Manager(app)
+
+@manager.command
+def test():
+    """Run the unit tests"""
+    tests=unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__ == '__main__':
     manager.run()
