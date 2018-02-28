91e19363e718ed7109109480a061ccc82e9d824c
gxywl <gxywl@163.com>
2018-03-01 01:59:52 +0800

    15 可运行出现主页,但测试没有错误（前setUp  no setup），有失败

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -6,7 +6,7 @@ from app import create_app
 
 
 class BasicsTestCase(unittest.TestCase):
-    def setup(self):
+    def setUp(self):
         self.app = create_app('testing')
         self.app_context = self.app.app_context()
         self.app_context.push()
