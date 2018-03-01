f5b6fe4f1813b3b9f2408dffed4d99915ac8754c
gxywl <gxywl@163.com>
2018-03-01 11:14:48 +0800

    17 修改视图index路由的相关用到的代码和文件，并迁移数据库，完成通过

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,32 @@
+from datetime import datetime
+
+from flask import session, flash, redirect, url_for, render_template, current_app
+
+from ..email import send_email
+from .. import db
+from ..models import User
+from .forms import NameForm
 from . import main
 
 
-@main.route('/')
+@main.route('/', methods=['Get', 'Post'])
 def index():
-    return 'hai'
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+            flash('Looks like you NEW!')
+            if current_app.config['DOFLASK_ADMIN']:
+                send_email(current_app.config['DOFLASK_ADMIN'], 'New User', 'mail/new_user', user=user)
+        else:
+            session['known'] = True
+            flash('Looks like you OLD!')
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('.index'))
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False),
+                           current_time=datetime.utcnow())
+
--- config.py
+++ config.py
@@ -5,18 +5,32 @@ basedir=os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY=os.environ.get('SECRET_KEY') or 'hard to guess string'
 
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    DOFLASK_MAIL_SUBJECT_PREFIX = '[DoFlask]'
+    DOFLASK_MAIL_SENDER = 'DoFlask Admin<50037521@qq.com>'
+    DOFLASK_ADMIN = os.environ.get('DOFLASK_ADMIN')
+
     @staticmethod
     def init_app(app):
         pass
 
 class DevelopmentConfig(Config):
     DEBUG=True
+    SQLALCHEMY_DATABASE_URI =os.environ.get('DEV_DATABASE_URL') or  'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+    MAIL_SERVER = 'smtp.qq.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
 class TestingConfig(Config):
     TESTING=True
 
 config={
-
+    'development': DevelopmentConfig,
     'testing':TestingConfig,
 
     'default': DevelopmentConfig
