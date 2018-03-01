54bb10ce756e4b48c363cbd5f7d602de9a303844
gxywl <gxywl@163.com>
2018-03-01 23:56:16 +0800

    23 修改模型 工厂 表单 路由，实现登入与登出

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
@@ -11,6 +12,10 @@ mail = Mail()
 moment=Moment()
 db=SQLAlchemy()
 
+login_manager=LoginManager()
+login_manager.session_protection='strong'
+login_manager.login_view='auth.login'
+
 def create_app(config_name):
     app=Flask(__name__)
     app.config.from_object(config[config_name])
@@ -20,6 +25,8 @@ def create_app(config_name):
     mail = Mail(app)
     moment = Moment(app)
     db.init_app(app)
+
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
