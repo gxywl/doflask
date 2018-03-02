c23eb6270e35977ba5e77e4d97020f376a372be3
gxywl <gxywl@163.com>
2018-03-02 23:24:37 +0800

    24 注册新用户 修改模型，添加令牌生成及用户确认，并做测试

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
+from flask import current_app
 from flask_login import UserMixin
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from . import db, login_manager
@@ -15,14 +17,16 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 
-class User(UserMixin,db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email=db.Column(db.String(64),unique=True,index=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
 
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     password_hash = db.Column(db.String(128))
+
+    confirmed = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -41,3 +45,19 @@ class User(UserMixin,db.Model):
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id})
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
