a2d19c7108e67820d1737d943e612aebac64b627
gxywl <gxywl@163.com>
2018-03-03 17:44:50 +0800

    24 注册新用户

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,5 +1,11 @@
 from flask import Blueprint
 
+from app.models import Permission
+
 main=Blueprint('main',__name__)
+
+@main.app_context_processor
+def inject_permissions():
+    return  dict(Perimission=Permission)
 
 from . import views, errors
